ebe9a825de6fe42bdc25d6ad535e7ceec2b16289
sql_ask_project <ahmedelsawy487@gmail.com>
2019-08-05 00:00:25 +0200

    friends bottom

--- app/src/main/java/com/elsawy/ahmed/sqlaskproject/Utils/Constants.java
+++ app/src/main/java/com/elsawy/ahmed/sqlaskproject/Utils/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
     public static final String URL_QUESTION = ROOT_URL+"handleQuestion.php";
+    public static final String URL_FRIENDS = ROOT_URL+"getFriends.php";
+
 }
---
+++ app/src/main/java/com/elsawy/ahmed/sqlaskproject/viewholder/FriendViewHolder.java
@@ -0,0 +1,59 @@
+package com.elsawy.ahmed.sqlaskproject.viewholder;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.elsawy.ahmed.sqlaskproject.R;
+import com.elsawy.ahmed.sqlaskproject.Utils.Utilties;
+import com.elsawy.ahmed.sqlaskproject.models.Friend;
+import com.elsawy.ahmed.sqlaskproject.models.Question;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class FriendViewHolder extends RecyclerView.ViewHolder{
+
+    private TextView friend_name;
+    private ImageView favorite_friend;
+    private CircleImageView profile_friend_image;
+    private ImageView accepted_image;
+    private CardView cardView;
+
+    public FriendViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        friend_name = (TextView) itemView.findViewById(R.id.friend_name);
+        favorite_friend = (ImageView) itemView.findViewById(R.id.favorite_friend);
+        accepted_image = (ImageView) itemView.findViewById(R.id.accepted_image_friend);
+        cardView = (CardView) itemView.findViewById(R.id.friend_item_card_view);
+        profile_friend_image = (CircleImageView) itemView.findViewById(R.id.profile_image_friend);
+    }
+    public void bindToFriend(Context context, Friend currentFriend){
+
+        friend_name.setText(currentFriend.getFriendName());
+        handleFavoriteImage(currentFriend.getFavorite());
+    }
+
+
+    private void handleFavoriteImage(Boolean isFavorite){
+        if (isFavorite)
+            favorite_friend.setImageResource(R.drawable.ic_stars_yellow_24dp);
+        else
+            favorite_friend.setImageResource(R.drawable.ic_star_black_24dp);
+
+//        favorite_friend.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (isFavorite)
+//                    FriendsAdapter.this.ref.child("user-objects").child("friends").child(FriendsAdapter.this.userData.getUid()).child(currentFriend.getFriendID()).child("favorite").setValue(false);
+//                else
+//                    FriendsAdapter.this.ref.child("user-objects").child("friends").child(FriendsAdapter.this.userData.getUid()).child(currentFriend.getFriendID()).child("favorite").setValue(true);
+//            }
+//        });
+    }
+}
